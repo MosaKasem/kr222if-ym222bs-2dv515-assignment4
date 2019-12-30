11c9713e5683a31fb89abaf8044118284ebd468a
MosaKasem <thescorpion119@gmail.com>
2019-12-30 19:58:18 +0100

    iterating, in progress

--- algorithm/NaiveBayesAlgo.js
+++ algorithm/NaiveBayesAlgo.js
@@ -20,7 +20,7 @@ const seperateByClass = (dataSet ,keyValue) => {
  * 
  * @param {array of arrays with objects inside} sorted 
  */
-const summarize = (sorted) => {
+const iterate_and_calc_mean = (sorted) => {
     const arrayOfMeansValue = []
     sorted.forEach((flowerType => {
         const currentFlowerType = flowerType[0]['4']
@@ -28,17 +28,33 @@ const summarize = (sorted) => {
         console.log('Flower Type: ', currentFlowerType);
         console.log('Flower Set Length: ', flowerType.length);
 
-        const meanResult = mean(flowerType)
+        const meanResult = calculate_mean(flowerType)
         arrayOfMeansValue.push({[currentFlowerType]: meanResult})
     }))
     return arrayOfMeansValue
 }
 
-const stdev = () => {
-
+const iterate_and_calc_std = numbers => {
+    const avg = iterate_and_calc_mean(numbers)
+    numbers.forEach(flowerSet => {
+        const res = calculate_stdev(flowerSet)
+    })
 }
 
-const mean = (objectSet) => {
+const calculate_stdev = (flowerSet) => {
+    let sum = []
+    flowerSet.map(e => {
+        console.log(flowerSet[0][4], e) // current iteration
+    })
+    // for (let i = 0; i < 4; i++) {
+    //     Object.keys(flowerSet).forEach(key => {
+    //         flowerSet[key][i]
+    //     })
+    // }
+    return sum
+}
+
+const calculate_mean = (objectSet) => {
     let sum = []
     let total = 0
     for (let i = 0; i < 4; i++) {
@@ -51,5 +67,6 @@ const mean = (objectSet) => {
     }
     return sum
 }
-const sorted = seperateByClass(iris, '4')
-summarize(sorted)
+const numbers = seperateByClass(iris, '4')
+// const mean = iterate_and_calculate(numbers)
+iterate_and_calc_std(numbers)
